b34a2092b5d077df7e8b8e254fa1b965d54e0aec
GoddessD <dlouis314@gmail.com>
2022-12-28 16:26:53 +0000

    second update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = (props) => {
+    const sixth = Math.floor(props.counter/100000)%10;
+    const fifth = Math.floor(props.counter/10000)%10;
+    const fourth = Math.floor(props.counter/1000)%10; 
+    const third = Math.floor(props.counter/100)%10;
+    const second = Math.floor(props.counter/10)%10;
+    const first = Math.floor(props.counter/1)%10;
+
+ return (<div className="entireCounter row"> 
+    <div className="digit col">
+	<i className="fas-regular fa-clock"></i>
+	</div>
+    <div className="digit col-sm">{sixth}</div>
+    <div className="digit col">{fifth}</div>
+    <div className="digit col">{fourth}</div>
+    <div className="digit col">{third}</div>
+    <div className="digit col">{second}</div>
+    <div className="digit col">{first}</div>
+    </div> 
+		 );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,9 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+    ReactDOM.render(<Home counter={counter}/>, document.querySelector("#app"));
+    counter ++;
+}, 1000);
+
